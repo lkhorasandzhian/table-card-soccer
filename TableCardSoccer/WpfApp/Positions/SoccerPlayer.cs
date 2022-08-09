0ae78e1fd886230f56c2d218ee9a5b598ac4b734
lkhorasandzhian <levon.kh228@gmail.com>
2022-08-10 00:50:44 +0300

    initial draft uploaded

---
+++ TableCardSoccer/WpfApp/Positions/SoccerPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Positions
+{
+    internal class SoccerPlayer
+    {
+        private uint _shoot;
+        private uint _pass;
+        private uint _defence;
+        private uint _stamina;
+
+        public uint Shoot { get { return _shoot; }}
+        public uint Pass { get { return _pass; }}
+        public uint Defence { get { return _defence; }}
+        public uint Stamina { get { return _stamina; }}
+    }
+}
